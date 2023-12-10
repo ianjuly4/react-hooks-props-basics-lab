829defbc6db414429f22d853327bc05db578f606
Ian July <ianjuly4@gmail.com>
2023-12-09 21:46:15 -0700

    props basic lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,12 +1,13 @@
-const user = {
-  name: "Liza",
-  city: "New York",
+
+  const user = {
+  name: "Ian",
+  city: "Denver",
   bio: "I made this!",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
     linkedin: "https://www.linkedin.com/in/liza/",
-  },
-};
+    },
+  }
 
 export default user;
